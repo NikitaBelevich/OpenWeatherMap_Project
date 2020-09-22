95ef626383b55d15fb83230593cae004fec635c9
Nikita Belevich <belevish_1998@mail.ru>
2020-09-22 10:14:14 +0300

    refactor: Изменена логика работы прелоадера
    Раньше, прелоадер включался только при переключении города из списка select. Он включался и через секунду выключался, что никак не было связано с фоновой загрузкой изображения.
    
    Теперь же, во-первых функция включения и выключения прелоадера была разбита на 2 функции. Во-вторых, теперь при загрузке страницы, а также при каждом переключении города асинхронно загружается фоновая картинка этого города. Это было реализовано в отдельной функции которая принимает аргументом url на фотографию города и создаёт новый узел-изображение. Она сначала включает прелоадер, затем ожидает загрузки картинки, если промис выполнился успешно, тогда мы добавляем эту загруженную картинку в блок weather, и выключается прелоадер. Если картинка недоступна, тогда мы просто добавляем обычный цвет фона этому блоку и у нас всё работает по прежнему.

--- js/app.js
+++ js/app.js
@@ -99,6 +99,54 @@ function getNameWind(windSpeed) {
 // погода на 5 дней ссылка
 // http://api.openweathermap.org/data/2.5/forecast?id=520555&lang=ru&appid=2570ad9f8710a971a6df178c71ad1705
 
+// https://wallbox.ru/wallpapers/main/201550/fe94967661b23ca.jpg
+
+
+
+const preloader = document.querySelector('.preloader');
+const containerWeather = document.querySelector('.weather');
+// Загружаем первый раз при загрузке страницы, т.к изначально данные стоят для Москвы
+awaitLoadPhotoOfCity('/img/Moscow.jpg');
+/*
+Функция принимает url картинки города, создаёт узел img и ждёт загрузки картинки, затем добавляет её в качестве фона.
+Изначально включается прелоадер, чтобы скрыть отрисовку, затем когда всё загрузилось, прелоадер выключается.
+*/
+function awaitLoadPhotoOfCity(urlCity) {
+    // Включаем прелоадер пока будем ждать загрузку картинки
+    enablePreloader(preloader);
+    
+    let promiseLoadImage = new Promise((resolve, reject) => {
+        const cityImg = new Image();
+        cityImg.src = urlCity;
+        cityImg.alt = '';
+        cityImg.classList.add('weather__background-city');
+        cityImg.onload = () => {resolve(cityImg);}
+        cityImg.onerror = () => {reject(new Error(`Image loading error: ${urlCity}`));}
+    });
+    promiseLoadImage
+        .then((img) => {
+            // Картинка загружена, вставляем на страницу
+            containerWeather.prepend(img);
+        })
+        .catch((err) => {
+            // Если не загрузилась фоновая картинка, даём обычный фон
+            console.error(err.message);
+            containerWeather.style.backgroundColor = '#3c7bde';
+        })
+        .finally(() => {
+            // При любых обстоятельствах закрываем прелоадер
+            setTimeout(() => {disablePreloader(preloader);}, 500);
+        })
+}
+function enablePreloader(preloader) {
+    preloader.style.transition = 'none';
+    preloader.classList.add('preloader-show');
+}
+function disablePreloader(preloader) {
+    preloader.classList.remove('preloader-show');
+    preloader.style.transition = '0.6s ease-out';
+}
+
 
 // первые 2 запроса выполняются при загрузке и выводят данные по погоде Москвы
 // запрос погоды на текущий момент---------------------------------------------------------------------------
@@ -353,7 +401,7 @@ fetch(`https://api.openweathermap.org/data/2.5/forecast?id=524901&lang=ru&appid=
 
         // объект с фотографиями городов
         const photosCities = {
-            'Москва': 'https://wallbox.ru/wallpapers/main/201550/fe94967661b23ca.jpg',
+            'Москва': '/img/Moscow.jpg',
             // https://avatars.mds.yandex.net/get-zen_doc/50509/pub_5cb067cb19feb400b5a02969_5cb078385c891100b324e6b5/scale_1200
             // https://www.ruspeach.com/upload/iblock/942/942212a3486b45bb5b0501d6cb11a10c.jpg
             'Санкт-Петербург': 'https://cdn.getyourguide.com/img/tour_img-1737994-148.jpg',
@@ -405,21 +453,9 @@ $(document).ready(function() {
     // выводим все данные по этому городу и стране 
     $('.weather-form__select').on('select2:selecting', function(e) {
 
-        // прелоадер
-        // включаем прелоадер
-        let preloader = document.querySelector('.preloader');
-            preloader.style.transition = 'none';
-            preloader.classList.toggle('preloader-show');
-        // через время скрываем его
-        setTimeout(function (){
-            preloader.classList.toggle('preloader-show');
-            preloader.style.transition = '0.6s ease-out';
-        }, 1000);
-        // прелоадер
-
         // console.log('Selecting: ' , e.params.args.data);
         const idCity = e.params.args.data.id;
-// запрос погоды на текущий момент---------------------------------------------------------------------------
+        // запрос погоды на текущий момент---------------------------------------------------------------------------
         // делаем запрос на текущую погоду при выборе города в select
         fetch(`https://api.openweathermap.org/data/2.5/weather?id=${idCity}&lang=ru&appid=2570ad9f8710a971a6df178c71ad1705`)
             .then(function (resp) {
@@ -435,12 +471,6 @@ $(document).ready(function() {
                 // console.warn(timezoneCity);
                 // вывод даты и времени города со смещением пояса в заголовок
                 
-                // определяем код страны и выводим соответствующий флаг
-                let imageFlag = document.querySelector('.country-city > img');
-                let codeState = data['sys']['country'];
-                imageFlag.src = flagsOfTheStates[codeState];
-                // определяем код страны и выводим соответствующий флаг
-                
                 // подстановка имени города
                 let nameCity = data.name;
                 let titleCity = document.querySelector('.country-city > div > span');
@@ -448,9 +478,16 @@ $(document).ready(function() {
                 // подстановка имени города
 
                 // выводим соответствующую городу фоновую фотографию 
-                let containerWeather = document.querySelector('.weather');
-                containerWeather.style.backgroundImage = `url(\'${photosCities[nameCity]}\')`;
+                // Удаляем картинку предыдущего города
+                document.querySelector('.weather__background-city').remove();
+                awaitLoadPhotoOfCity(photosCities[nameCity]);
                 // выводим соответствующую городу фоновую фотографию 
+
+                // определяем код страны и выводим соответствующий флаг
+                let imageFlag = document.querySelector('.country-city > img');
+                let codeState = data['sys']['country'];
+                imageFlag.src = flagsOfTheStates[codeState];
+                // определяем код страны и выводим соответствующий флаг
 
                 // подстановка иконки погоды
                 let weatherIcon = document.querySelector('#weather-icon');
